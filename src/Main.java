e6fc237921edba0878776301c0d9e1b365c28718
ulyana <bsergeybabak2000@gmail.com>
2023-03-02 12:52:16 +0300

    второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-      task1();
-
+        task1();
+        task2();
     }
-    public static void task1(){
 
+    public static void task1() {
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите приложение для IOS по ссылке");
+        } else {
+            System.out.println("Установите приложение для Android по ссылке");
+        }
     }
+
+    public static void task2() {
+        int clientDeviceYear = 2013;
+        switch (clientDeviceYear) {
+            case 2016:
+                System.out.println("Установите обычную версию приложения по ссылке");
+            case 2013:
+                System.out.println("Установите облегченную версию приложения по ссылке");
+        }
+    }
+
 }
